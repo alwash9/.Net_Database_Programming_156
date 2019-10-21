c2a7929b508b9bb17dfc1661c575dc65002689e4
BIT student <bitstudent@bitnet.wctc.edu>
2019-10-21 18:39:28 -0500

    Add project files.

---
+++ Entity-ClassDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entity_ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
