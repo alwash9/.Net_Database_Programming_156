35c3e894761e145da6927c193c8f294c438bf8e7
Albert Washington <awashington9@my.wctc.edu>
2019-10-21 20:00:58 -0500

    Break time save in case power failure.

--- Entity-ClassDemo/Models/Blog.cs
+++ Entity-ClassDemo/Models/Blog.cs
@@ -6,5 +6,10 @@ namespace Entity_ClassDemo.Models
 {
     public class Blog
     {
+        public int BlogId { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Post> Posts { get; set; }
     }
 }
--- Entity-ClassDemo/Models/Post.cs
+++ Entity-ClassDemo/Models/Post.cs
@@ -6,5 +6,11 @@ namespace Entity_ClassDemo.Models
 {
     public class Post
     {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+
     }
 }
